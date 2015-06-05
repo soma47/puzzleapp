d21e15fe45756276c39297d0d9de97ceac849508
Soma <soma4747@gmail.com>
2015-06-05 17:55:52 +0900

    Initial Commit

---
+++ originalapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  originalapp
+//
+//  Created by Soma on 2015/06/05.
+//  Copyright (c) 2015年 Soma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
