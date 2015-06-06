e79d5691217dbf2115c4cb87f2c9297fd29d8d33
Soma <soma4747@gmail.com>
2015-06-06 19:07:19 +0900

    オリジナルアプリ１回目のコミットです。

--- originalapp/ViewController.swift
+++ originalapp/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var ticTacBtn1 : UIButton!
+    @IBOutlet var ticTacBtn2 : UIButton!
+    @IBOutlet var ticTacBtn3 : UIButton!
+    @IBOutlet var ticTacBtn4 : UIButton!
+    @IBOutlet var ticTacBtn5 : UIButton!
+    @IBOutlet var ticTacBtn6 : UIButton!
+    @IBOutlet var ticTacBtn7 : UIButton!
+    @IBOutlet var ticTacBtn8 : UIButton!
+    @IBOutlet var ticTacBtn9 : UIButton!
+
+    @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet var remainLabel: UILabel!
+        var banArray:[[String]] = [["0","0","0","0","1"] ,["0","0","0","0","0"],["0","0","0","0","0"] ,["0","0","0","0","0"] ,["0","0","0","0","0"],["0","0","0","0","2"]]
+        let EMPTY = 0, PLAYER1 = 1, PLAYER2 = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +33,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func btClick(sender: UIButton){
+        banArray[0][sender.tag-1] = 1
+        
+        
+    }
+    @IBAction func btClick2(sender: UIButton){
+        banArray[1][sender.tag-1] = 1
+        
+        
+    }
+    @IBAction func btClick3(sender: UIButton){
+        banArray[2][sender.tag-1] = 1
+        
+        
+    }
+    @IBAction func btClick4(sender: UIButton){
+        banArray[3][sender.tag-1] = 1
+        
+        
+    }
+    @IBAction func btClick5(sender: UIButton){
+        banArray[4][sender.tag-1] = 1
+        
+        
+    }
 }
+
+
+
+
 
